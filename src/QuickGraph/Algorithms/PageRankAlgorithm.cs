10080c99dbf27316f88dcf4359234a5e005cf653
CP\NAnestev <nikolay.anestev@onalytica.com>
2017-11-16 11:41:54 +0000

    nuspec file added.

--- src/QuickGraph/Algorithms/PageRankAlgorithm.cs
+++ src/QuickGraph/Algorithms/PageRankAlgorithm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using QuickGraph.Predicates;
 using QuickGraph.Collections;
@@ -12,17 +13,17 @@ namespace QuickGraph.Algorithms.Ranking
         AlgorithmBase<IBidirectionalGraph<TVertex, TEdge>>
         where TEdge : IEdge<TVertex>
     {
-        private IDictionary<TVertex,double> ranks = new Dictionary<TVertex,double>();
+        private IDictionary<TVertex, double> ranks = new Dictionary<TVertex, double>();
 
         private int maxIterations = 60;
         private double tolerance = 2 * double.Epsilon;
         private double damping = 0.85;
 
         public PageRankAlgorithm(IBidirectionalGraph<TVertex, TEdge> visitedGraph)
-            :base(visitedGraph)
-        {}
+            : base(visitedGraph)
+        { }
 
-        public IDictionary<TVertex,double> Ranks
+        public IDictionary<TVertex, double> Ranks
         {
             get
             {
@@ -68,57 +69,56 @@ namespace QuickGraph.Algorithms.Ranking
 
         public void InitializeRanks()
         {
-            this.ranks.Clear();
-            foreach (var v in this.VisitedGraph.Vertices)
-            {
-                this.ranks.Add(v, 0);
-            }
-//            this.RemoveDanglingLinks();
+            this.ranks = this.VisitedGraph.Vertices.ToDictionary(v => v, v => 0D);
+            //foreach (var v in this.VisitedGraph.Vertices)
+            //{
+            //    this.ranks.Add(v, 0);
+            //}
+
+            //            this.RemoveDanglingLinks();
         }
-/*
-        public void RemoveDanglingLinks()
-        {
-            VertexCollection danglings = new VertexCollection();
-            do
-            {
-                danglings.Clear();
-
-                // create filtered graph
-                IVertexListGraph fg = new FilteredVertexListGraph(
-                    this.VisitedGraph,
-                    new InDictionaryVertexPredicate(this.ranks)
-                    );
-
-                // iterate over of the vertices in the rank map
-                foreach (IVertex v in this.ranks.Keys)
+        /*
+                public void RemoveDanglingLinks()
                 {
-                    // if v does not have out-edge in the filtered graph, remove
-                    if (fg.OutDegree(v) == 0)
-                        danglings.Add(v);
-                }
+                    VertexCollection danglings = new VertexCollection();
+                    do
+                    {
+                        danglings.Clear();
 
-                // remove from ranks
-                foreach (IVertex v in danglings)
-                    this.ranks.Remove(v);
-                // iterate until no dangling was removed
-            } while (danglings.Count != 0);
-        }
-*/
+                        // create filtered graph
+                        IVertexListGraph fg = new FilteredVertexListGraph(
+                            this.VisitedGraph,
+                            new InDictionaryVertexPredicate(this.ranks)
+                            );
+
+                        // iterate over of the vertices in the rank map
+                        foreach (IVertex v in this.ranks.Keys)
+                        {
+                            // if v does not have out-edge in the filtered graph, remove
+                            if (fg.OutDegree(v) == 0)
+                                danglings.Add(v);
+                        }
+
+                        // remove from ranks
+                        foreach (IVertex v in danglings)
+                            this.ranks.Remove(v);
+                        // iterate until no dangling was removed
+                    } while (danglings.Count != 0);
+                }
+        */
         protected override void InternalCompute()
         {
             var cancelManager = this.Services.CancelManager;
             IDictionary<TVertex, double> tempRanks = new Dictionary<TVertex, double>();
 
             // create filtered graph
-            FilteredBidirectionalGraph<
-                TVertex,
-                TEdge,
-                IBidirectionalGraph<TVertex,TEdge>
-                > fg = new FilteredBidirectionalGraph<TVertex, TEdge, IBidirectionalGraph<TVertex, TEdge>>(
+            var fg = new FilteredBidirectionalGraph<TVertex, TEdge, IBidirectionalGraph<TVertex, TEdge>>(
                 this.VisitedGraph,
-                new InDictionaryVertexPredicate<TVertex,double>(this.ranks).Test,
+                new InDictionaryVertexPredicate<TVertex, double>(this.ranks).Test,
                 e => true
-                );
+            );
+            var inEdges = fg.Vertices.ToDictionary(v => v, v => fg.InEdges(v).ToArray());
+            var outDegrees = fg.Vertices.ToDictionary(v => v, v => fg.OutDegree(v));
 
             int iter = 0;
             double error = 0;
@@ -126,28 +126,31 @@ namespace QuickGraph.Algorithms.Ranking
             {
                 if (cancelManager.IsCancelling)
                     return;
-                  
+
                 // compute page ranks
                 error = 0;
-                foreach (KeyValuePair<TVertex,double> de in this.Ranks)
+                var ranksArr = this.Ranks.ToArray();
+                var ranksArrInx = ranksArr.Count() - 1;
+                while (ranksArrInx > -1 && !cancelManager.IsCancelling)
                 {
-                    if (cancelManager.IsCancelling)
-                        return;
+                    var de = ranksArr[ranksArrInx--];
 
-                    TVertex v = de.Key;
-                    double rank = de.Value;
                     // compute ARi
                     double r = 0;
-                    foreach (var e in fg.InEdges(v))
+                    var edges = inEdges[de.Key];
+                    int edgesInx = edges.Count() - 1;
+                    while (edgesInx > -1)
                     {
-                        r += this.ranks[e.Source] / fg.OutDegree(e.Source);
+                        var e = edges[edgesInx--];
+                        r += this.ranks[e.Source] / outDegrees[e.Source];
                     }
 
                     // add sourceRank and store
                     double newRank = (1 - this.damping) + this.damping * r;
-                    tempRanks[v] = newRank;
+                    tempRanks[de.Key] = newRank;
+
                     // compute deviation
-                    error += Math.Abs(rank - newRank);
+                    error += Math.Abs(de.Value - newRank);
                 }
 
                 // swap ranks
@@ -156,18 +159,15 @@ namespace QuickGraph.Algorithms.Ranking
                 tempRanks = temp;
 
                 iter++;
-            } while (error > this.tolerance && iter < this.maxIterations);
+            }
+            while (error > this.tolerance && iter < this.maxIterations);
+
             Console.WriteLine("{0}, {1}", iter, error);
         }
 
         public double GetRanksSum()
         {
-            double sum = 0;
-            foreach (double rank in this.ranks.Values)
-            {
-                sum += rank;
-            }
-            return sum;
+            return this.ranks.Values.Sum();
         }
 
         public double GetRanksMean()
